f0afb59534887041d47ecdf398dc9f30a16feb08
Pawani Muthusala Perera <89435818+ashen910@users.noreply.github.com>
2022-11-12 11:47:23 +0530

    new

--- app/src/main/java/com/example/white_light/PTouchHelper.java
+++ app/src/main/java/com/example/white_light/PTouchHelper.java
@@ -17,32 +17,6 @@ public class PTouchHelper extends ItemTouchHelper.SimpleCallback {
         this.adapter = adapter;
     }
 
-    @Override
-    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
-        return false;
-    }
-
-    @Override
-    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-        final int position = viewHolder.getAdapterPosition();
-        if (direction == ItemTouchHelper.LEFT){
-            adapter.updateData(position);
-            adapter.notifyDataSetChanged();
-        }else{
-            adapter.deleteData(position);
-        }
-    }
-
-    @Override
-    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
-
-        new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
-                .addSwipeRightBackgroundColor(R.color.purple_500)
-                .addSwipeRightActionIcon(R.drawable.ic_baseline_delete_24)
-                .addSwipeLeftBackgroundColor(R.color.purple_500)
-                .addSwipeLeftActionIcon(R.drawable.ic_baseline_edit_24)
-                .create()
-                .decorate();
 
         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
     }
