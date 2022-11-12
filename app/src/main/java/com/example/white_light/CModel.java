ec2321f3f6d1900d0689be0500f0ea2ba83fd63c
Pawani Muthusala Perera <89167761+Chathum10@users.noreply.github.com>
2022-11-12 11:29:05 +0530

    update

--- app/src/main/java/com/example/white_light/CModel.java
+++ app/src/main/java/com/example/white_light/CModel.java
@@ -12,30 +12,7 @@ public class CModel {
         this.weight= weight;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
-    public String getHeight() {
-        return height;
-    }
-
-    public void setHeight(String height) {
-        this.height = height;
-    }
-
+    
     public String getWeight() {
         return weight;
     }
